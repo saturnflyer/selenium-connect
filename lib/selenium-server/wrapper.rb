0785fa9a36e54e2318a9c3a03d1e27aa8ae8a287
Dave Haeffner <dave@arrgyle.com>
2013-04-16 17:29:45 -0400

    Updated the rake_file loader to use the proper path, fixing a bug

--- lib/selenium-server/wrapper.rb
+++ lib/selenium-server/wrapper.rb
@@ -14,7 +14,7 @@ module SeleniumServer
   private
 
   def self.rake_file
-    "#{Dir.pwd}/lib/selenium-server/rake.task"
+    "#{File.join(File.dirname(File.expand_path(__FILE__)), 'rake.task')}"
   end
 
   def self.rake(task)
