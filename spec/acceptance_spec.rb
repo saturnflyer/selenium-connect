cfaf2eece1418ece0eaa9bd9514b6faa04f9ffb4
Dave Haeffner <dave@arrgyle.com>
2013-05-10 15:02:45 -0400

    Abstracted Firefox functionality out into its own class within Runner. Also added scaffolding for IE support. Lastly, fixed a bug by removing a hard-code dependency to the Remote::Bridge class which was done based on a faulty assumption.

---
+++ spec/acceptance_spec.rb
@@ -0,0 +1,73 @@
+require 'selenium-connect'
+
+describe "Acceptance Tests" do
+
+  let(:google)  { Google.new(SeleniumConnect.start) }
+  let(:driver)  { SeleniumConnect.start }
+  let(:quit)    { SeleniumConnect.finish }
+
+  context "Common" do
+    it "logging", :wip => true do
+      pending ('needing it')
+      SeleniumConnect.configure do |c|
+        c.log = "#{Dir.pwd}/runner.out"
+      end
+      google.visit
+      SeleniumConnect.finish
+      File.read('runner.out').empty?.should == false
+      File.delete('runner.out')
+    end
+  end
+
+  context "Firefox" do
+    it "blank config" do
+      SeleniumConnect.configure do end
+      google.visit
+      google.page_title.should include("Google")
+      quit
+    end
+
+    it "localhost" do
+      SeleniumConnect.configure do |c|
+        c.host  = "localhost"
+      end
+      google.visit
+      google.page_title.should include("Google")
+      quit
+    end
+
+    it "profile name", :wip => true do
+#      pending "requires machine setup to run, and need a public example"
+      SeleniumConnect.configure do |c|
+        c.profile_name  = "nt50_ic_egroup"
+      end
+      driver.get 'https://mashup.ic.mantech.com/ispace'
+      driver.find_element(css: "#navCreate").click
+      quit
+    end
+
+    it "profile path" do
+      pending "need to add a profile to the repo"
+      SeleniumConnect.configure do |c|
+        c.profile_path  = "#{Dir.pwd}/path/to/profile"
+      end
+    end
+
+  end #Context//Firefox
+end #Describe
+
+class Google
+  attr_accessor :page
+
+  def initialize(driver)
+    @page = driver
+  end
+
+  def visit
+    page.get "http://www.google.com"
+  end
+
+  def page_title
+    page.getTitle
+  end
+end
