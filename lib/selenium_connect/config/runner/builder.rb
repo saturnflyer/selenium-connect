adbf601ff07bc3cd92d3958a6b3ac86e0932ddee
Jim Gay <jim@saturnflyer.com>
2014-04-07 23:03:50 -0400

    allow alternate configuration types to be used

--- lib/selenium_connect/config/runner/builder.rb
+++ lib/selenium_connect/config/runner/builder.rb
@@ -10,20 +10,18 @@ module SeleniumConnect
       module Builder
         module_function
 
-      def build(opts = {})
-        defaults = { type: 'local', options: {} }
-        opts = defaults.merge opts
-        case opts[:type]
-        when 'sauce'
-          Sauce.new opts[:options]
-        when 'remote'
-          Remote.new opts[:options]
-        when 'local'
-          Local.new opts[:options]
-        else
+        def build(opts = {})
+          defaults = { type: 'local', options: {} }
+          config = defaults.merge opts
+
+          type = config.type
+          runner_type = type.to_s.gsub(/(?:^|_)([a-z])/){ $1.upcase }
+          runner_class = const_get("SeleniumConnect::Config::Runner::#{runner_type}")
+          runner_class.new(config.fetch(:options))
+        rescue NameError
           fail ArgumentException, "Unknown type: \"#{type}\" passed to runner config builder."
         end
-      end
+
       end
     end
   end
